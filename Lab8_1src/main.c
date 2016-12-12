3ec8f8437a692fb02f0da2b627cdb41e490eb1a1
unknown <>
2016-12-13 01:58:48 +0800

    hi

--- Lab8_1src/main.c
+++ Lab8_1src/main.c
@@ -5,6 +5,35 @@
 
 extern void GPIO_init(void);
 
+void clock_init(){ // 1Mhz
+
+  // hsi on and switch to hsi
+  RCC->CR |= RCC_CR_HSION;
+  while((RCC->CR & RCC_CR_HSIRDY) == 0); // check hsi ready
+  RCC->CFGR |= 0b01;
+  RCC->CFGR &= 0xFFFFFFF1;
+  while ((RCC->CFGR & 0b11) != 0b01);
+
+  // Turn PLL off
+    RCC->CR &= ~RCC_CR_PLLON;
+    while(RCC->CR & RCC_CR_PLLRDY);
+
+  RCC->CFGR |= 0b1010<<4;
+  RCC->CFGR &= 0xFFFFFFAF;
+
+  RCC->PLLCFGR |= 0b001000000000001010000010010;
+  RCC->PLLCFGR &= 0b11111001111111111001010010011110;
+
+  RCC->CR |= RCC_CR_PLLON;
+  while((RCC->CR & RCC_CR_PLLRDY) == 0);
+
+  RCC->CFGR |= RCC_CFGR_SW_PLL;
+    while ((RCC->CFGR & RCC_CFGR_SWS_PLL) != RCC_CFGR_SWS_PLL);
+
+  return;
+}
+
+
 void SystemClock_Config(){
  //TODO: Setup system clock and SysTick timer interrupt
 
@@ -13,7 +42,7 @@ void SystemClock_Config(){
 	SysTick->CTRL |= (uint32_t)0x00000004; //Set CLKSOURCE to 1b - processor clock (0b - processor clock / 16)
 	SysTick->CTRL |= (uint32_t)0x00000002; //Set TICKINT to 1b - enable interrupt on wrap.
 	SysTick->LOAD &= (uint32_t)0xFF000000;
-	SysTick->LOAD |= (uint32_t)2000000; //Set Reload Register to 48000d -> 1ms.
+	SysTick->LOAD |= (uint32_t)5000000; //Set Reload Register to 48000d -> 1ms.
 										  //so *500 => 24000000d-> 500ms=0.5sec
 
 	//SysTick->CTRL &= ~(uint32_t)0x00000001; //Clear ENABLE to 1b - enable SYSTICK.
@@ -45,8 +74,10 @@ int user_press_button(){
 
 
 int main(){
+	clock_init();
 	SystemClock_Config();
 	GPIO_init();
+
 	while(1){
 		if(user_press_button()){
 			//TODO: Enable or disable Systick timer
