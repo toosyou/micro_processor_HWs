b25516ff64eb12826479ab56c497933de5eef2eb
Evan Chang <toosyou70207@gmail.com>
2016-12-19 01:37:28 +0800

    code clean up

--- Lab9_1src/main.c
+++ Lab9_1src/main.c
@@ -10,32 +10,12 @@ typedef unsigned int bool;
 extern void GPIO_init();
 
 
-int WriteToLCD(int input, int isCmd) {
+void WriteToLCD(int input, int isCmd){
 
-	//TODO: Write command to LCD or let LCD display character fetched from memory.
-}
-void WriteToLCD(int input, int isCmd)
-{
-	//TODO: Write command to LCD or let LCD display character fetched from memory.
-	//...
-	TM_GPIO_SetPinLow(GPIOB, LCD_RSPin);
-	//...
-	TM_GPIO_SetPinHigh(GPIOB, LCD_RSPin);
-	TM_GPIO_SetPinLow(GPIOB, LCD_RWPin);
+    
 
-	for (int index = 0 ; index < 8 ; index++)
-	{
-		//...
-		TM_GPIO_SetPinHigh(GPIOA, LCD_dataPin[index]);
-		//...
-		TM_GPIO_SetPinLow(GPIOA, LCD_dataPin[index]);
-		//...
-	}
-	TM_GPIO_SetPinHigh(GPIOB, LCD_ENPin);
-	delay_ms(10);
-	TM_GPIO_SetPinLow(GPIOB, LCD_ENPin);
-	delay_ms(10);
 
+    return;
 }
 void init_LCD() {
 
@@ -52,20 +32,9 @@ void SysTick_Handler() {
 
 }
 
-void init() {
-	for (int i = 0; i < 8; i++) {
-		TM_GPIO_Init(GPIOA, LCD_dataPin[i], TM_GPIO_Mode_OUT, TM_GPIO_OType_PP,
-		TM_GPIO_PuPd_UP, TM_GPIO_Speed_Medium);
-	}
-	TM_GPIO_Init(GPIOB, LCD_VSPin, TM_GPIO_Mode_OUT, TM_GPIO_OType_PP,TM_GPIO_PuPd_UP, TM_GPIO_Speed_Medium);
-	TM_GPIO_Init(GPIOB, LCD_RSPin, TM_GPIO_Mode_OUT, TM_GPIO_OType_PP,TM_GPIO_PuPd_UP, TM_GPIO_Speed_Medium);
-	TM_GPIO_Init(GPIOB, LCD_RWPin, TM_GPIO_Mode_OUT, TM_GPIO_OType_PP,TM_GPIO_PuPd_UP, TM_GPIO_Speed_Medium);
-	TM_GPIO_Init(GPIOB, LCD_ENPin, TM_GPIO_Mode_OUT, TM_GPIO_OType_PP,TM_GPIO_PuPd_UP, TM_GPIO_Speed_Medium);
-	init_LCD();
-}
-
 int main() {
-	init();
+    GPIO_init();
+	init_LCD();
 	SysTick_Config(1300000UL);
 	while(1){
 
