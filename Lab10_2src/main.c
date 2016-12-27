4bdb670f063ab0e09afe68ed94e61add6a332627
Evan Chang <toosyou70207@gmail.com>
2016-12-28 02:43:11 +0800

    lab10_2 done

--- Lab10_2src/main.c
+++ Lab10_2src/main.c
@@ -8,6 +8,169 @@ typedef unsigned int bool;
 #define false 0
 #define true 1
 
+float resistor = 0.0f;
+
+void set_baud_rate_uart(USART_TypeDef *usart, int fck, int baud){
+	usart->BRR = (fck + baud / 2) / baud;
+	return;
+}
+
+void set_length_stop_uart(USART_TypeDef *usart, int length){
+	//0 -> 1
+	//1 -> 0.5
+	//2 -> 2
+	//3 -> 1.5
+	usart->CR2 &= ~USART_CR2_STOP_Msk;
+	usart->CR2 |= length << USART_CR2_STOP_Pos;
+	return;
+}
+
+void enable_rt_usart(USART_TypeDef *usart, int rx, int tx){
+	usart->CR1 &= ~(3 << 2); // clear
+	usart->CR1 |= rx << USART_CR1_RE_Pos;
+	usart->CR1 |= tx << USART_CR1_TE_Pos;
+	return;
+}
+
+void enable_usart(USART_TypeDef *usart){
+	usart->CR1 |= USART_CR1_UE;
+	return;
+}
+
+
+void set_length_word_uart(USART_TypeDef *usart, int length){
+	if(length == 7){ // 10
+		usart->CR1 |= USART_CR1_M1;
+		usart->CR1 &= ~USART_CR1_M0;
+	}
+	else if(length == 8){ // 00
+		usart->CR1 &= ~USART_CR1_M1;
+		usart->CR1 &= ~USART_CR1_M0;
+	}
+	else if(length == 9){ // 01
+		usart->CR1 &= ~USART_CR1_M1;
+		usart->CR1 |= USART_CR1_M0;
+	}
+	return;
+}
+
+
+int flag_status_usart(USART_TypeDef *usart, int flag){
+	return usart->ISR & flag;
+}
+
+int UART_Transmit(USART_TypeDef *usart, char *arr, uint32_t size) {
+
+	for(unsigned int i=0;i<size;++i){
+		//Transmit data register empty
+		while(!flag_status_usart(usart, USART_ISR_TXE));
+
+		usart->TDR = arr[i];
+		//Transmission complete
+		while(!flag_status_usart(usart, USART_ISR_TC));
+	}
+
+	return 0;
+}
+
+
+void set_resolution_adc(ADC_TypeDef *adc, int resolution){
+	// 12 -> 00
+	// 10 -> 01
+	// 8 -> 10
+	// 6 -> 11
+	adc->CFGR &= ~ADC_CFGR_RES; //clear
+	adc->CFGR |= ((12-resolution)/2) << ADC_CFGR_RES_Pos;
+	return;
+}
+
+void enable_continuous_convetion_adc(ADC_TypeDef *adc, bool enable){
+	if(enable == true){
+		adc->CFGR |= ADC_CFGR_CONT;
+	}else{
+		adc->CFGR &= ~ADC_CFGR_CONT;
+	}
+	return ;
+}
+
+void set_data_align_adc(ADC_TypeDef *adc, bool left){
+	// false -> right
+	// true -> left
+	if(left) // left
+		adc->CFGR |= ADC_CFGR_ALIGN;
+	else
+		adc->CFGR &= ~ADC_CFGR_ALIGN;
+	return;
+}
+
+void enable_regulator_adc(ADC_TypeDef *adc){
+	adc->CR &= ~ADC_CR_DEEPPWD;
+	adc->CR |= ADC_CR_ADVREGEN;
+	for(int i=0;i<500;++i);//do nothing
+	return;
+}
+
+void enable_adc(ADC_TypeDef *adc){
+	adc->CR |= ADC_CR_ADEN;
+	while(!(ADC1->ISR & ADC_ISR_ADRDY)); // wait for enable
+	return;
+}
+
+int read_adc(ADC_TypeDef *adc){
+	return adc->DR;
+}
+
+void convert_adc(ADC_TypeDef *adc){
+	adc->CR |= ADC_CR_ADSTART;
+	return;
+}
+
+void set_interrupt_adc(ADC_TypeDef *adc, unsigned int pos_mask, bool enable){
+	if(enable)
+		adc->IER |= pos_mask;
+	else
+		adc->IER &= ~pos_mask;
+	return;
+}
+
+void set_convert_mode_adc(int mode){
+	ADC123_COMMON->CCR &= ~ADC_CCR_DUAL;
+	ADC123_COMMON->CCR |= mode << ADC_CCR_DUAL_Pos;
+	return;
+}
+
+void set_clock_mode_adc(int mode){
+	ADC123_COMMON->CCR &= ~ADC_CCR_CKMODE;
+	ADC123_COMMON->CCR |= mode << ADC_CCR_CKMODE_Pos;
+	return;
+}
+
+void set_prescaler_adc(int prescaler){
+	ADC123_COMMON->CCR &= ~ADC_CCR_PRESC;
+	ADC123_COMMON->CCR |= prescaler << ADC_CCR_PRESC_Pos;
+}
+
+void set_dma_mode_adc(int mode){
+	ADC123_COMMON->CCR &= ~ADC_CCR_MDMA;
+	ADC123_COMMON->CCR |= mode << ADC_CCR_MDMA_Pos;
+}
+
+void set_delay_adc(int delay_clk){
+	ADC123_COMMON->CCR &= ~ADC_CCR_DELAY;
+	ADC123_COMMON->CCR |= delay_clk << ADC_CCR_DELAY_Pos;
+}
+
+void set_channel_adc(ADC_TypeDef *adc, int channel, int rank, int sampleTime){
+	if(rank <= 4){
+		adc->SQR1 &= ~(ADC_SQR1_SQ1 << (rank * 6));
+		adc->SQR1 |= (channel << (rank * 6));
+	}
+	if(channel <= 9){
+		adc->SMPR1 &= ~(ADC_SMPR1_SMP0 << (channel * 3));
+		adc->SMPR1 |= (sampleTime << (channel * 3));
+	}
+}
+
 void SysTickConfig(int tick){
 	SysTick->CTRL &= ~(SysTick_CTRL_ENABLE_Msk);
 	SysTick->LOAD = tick & SysTick_LOAD_RELOAD_Msk;
@@ -18,9 +181,16 @@ void SysTickConfig(int tick){
 }
 
 void SysTick_Handler(){
-
+	convert_adc(ADC1);
+	return;
 }
 
+void ADC1_2_IRQHandler(void){
+	while(!(ADC1->ISR&ADC_ISR_EOC)); // wait for complete
+	float voltage = (float)read_adc(ADC1) / 4096.0f * 5.0f;
+	resistor = ( 5.0f - voltage ) * 10000.0f / voltage;
+	return;
+}
 void GPIO_Init(void) {
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
@@ -83,33 +253,19 @@ void ADC_init(void){
 	GPIOC->MODER |= 0b11;
 	GPIOC->ASCR |= 1;
 	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;
-	ADC_Resolution(ADC1, 0); // 12 bits
-	ADC_ContinuousConversion(ADC1, 0); // enable continuous conversion
-	ADC_DataAlign(ADC1, 0); // set right align
-	ADC_CommonDualMode(0); // independent mode
-	ADC_CommonClockMode(1); // hclk / 1
-	ADC_CommonPrescaler(0); //div 1
-	ADC_CommonDMAMode(0); // disable dma
-	ADC_CommonDelayTwoSampling(0b0100); // 5 adc clk cycle
-	ADC_Channel(ADC1, 1, 1, 2); // channel 1, rank 1, 12.5 adc clock cycle
-	ADC_Wakeup(ADC1);
-	ADC_Interrupt(ADC1, ADC_IER_EOCIE, 1);
+	set_resolution_adc(ADC1, 12); // 12 bits
+	enable_continuous_convetion_adc(ADC1, false); // enable continuous conversion
+	set_data_align_adc(ADC1, false); // set right align
+	set_convert_mode_adc(0); // independent mode
+	set_clock_mode_adc(1); // hclk / 1
+	set_prescaler_adc(0); //div 1
+	set_dma_mode_adc(0); // disable dma
+	set_delay_adc(0b0100); // 5 adc clk cycle
+	set_channel_adc(ADC1, 1, 1, 2); // channel 1, rank 1, 12.5 adc clock cycle
+	enable_regulator_adc(ADC1);
+	set_interrupt_adc(ADC1, ADC_IER_EOCIE, 1);
 	NVIC_EnableIRQ(ADC1_2_IRQn);
-	ADC_Enable(ADC1);
-}
-
-void TWO(void){
-	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;	//PC enable
-	GPIOC->MODER &= ~(0b11 << (13 * 2));	//PC13
-	int last = 0;
-	while(1){
-		if(( bottom_clicked() ) && !last){
-			char s[20];
-			sprintf(s, "%f\n", resistor);
-			USARTPrintString(USART1, s);
-		}
-		last = x;
-	}
+	enable_adc(ADC1);
 }
 
 int main(){
@@ -118,14 +274,14 @@ int main(){
 	SysTickConfig(40000);
 	usart_init();
 	ADC_init();
-	TWO();
-	/*
 	while(1){
 		if(bottom_clicked() == true){
-			char message[50] = "HeI10, W0r1d!";
+			char message[50] = {0};
+			sprintf(message, "%f Ω\t", resistor);
+			UART_Transmit(USART1, message, strlen(message));
+			sprintf(message, "%f V\t", (float)read_adc(ADC1) / 4096.0f * 5.0f);
 			UART_Transmit(USART1, message, strlen(message));
 		}
 	}
-	*/
 	return 0;
 }
